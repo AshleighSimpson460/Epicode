7a833642ae90a84ff025634dd7d0aa62b92760b6
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-01-22 21:31:36 +0000

    uploading some changes / incomplete build

--- M2/D4/index.js
+++ M2/D4/index.js
@@ -84,7 +84,13 @@
    Write a piece of code for deleting only even entries from an array.
   */
   
-    const array5 = [1,2,3,4,5,6,7,8,9]
+   const arr4 = [1,2,3,4,5,6]
+   for(let i = 0; i < arr4.length; i++){
+     if(arr4[i] % 2 ===0){
+         arr4.splice(i--,1)
+     }
+   }
+   console.log(arr4)
 
 
 
