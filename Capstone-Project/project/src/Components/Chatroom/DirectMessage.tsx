67e79cf6c45f77f5d63e821ee7db728cc5561e68
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-07-04 13:10:07 +0100

    added customization for the chatroom

--- Capstone-Project/project/src/Components/Chatroom/DirectMessage.tsx
+++ Capstone-Project/project/src/Components/Chatroom/DirectMessage.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 
-const DirectMessage = ({ match }) => {
+const DirectMessage = ({ match, socket }) => {
   const chatroomId = match.params.Id;
-  return <div>DirectMessage</div>;
+  return (
+    <div className="chatroomPage">
+      <div className="chatroomSection">
+        <div className="cardHeader">
+          <div className="chatroomContent">
+            <div className="message">
+              <span className="otherMessage"></span>
+              <span className="ownMessage"></span>
+            </div>
+          </div>
+          <div className="chatroomActions">
+            <div>
+              <input
+                type="text"
+                name="message"
+                placeholder="enter message"
+                id=""
+              />
+            </div>
+            <div>
+              <button className="join">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DirectMessage;
