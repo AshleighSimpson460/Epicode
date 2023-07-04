f9990b8620071e468a7cbba82fc9d0ec12892d99
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-07-04 12:55:30 +0100

    connected socket io to backend and frontend

--- Capstone-Project/Backend/index.js
+++ Capstone-Project/Backend/index.js
@@ -2,19 +2,25 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import jwt from "jwt-then";
+import jwt from "jsonwebtoken";
 import http from "http";
+import { Server } from "socket.io";
 
 import { router as restaurantRouter } from "./Routes/router.js";
 import { router as userRouter } from "./Routes/user.js";
 import { router as chatroomRouter } from "./Routes/chatroom.js";
-import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 app.use(
   cors({
@@ -23,42 +29,42 @@ app.use(
   })
 );
 
+app.use(express.json());
+
 app.use("/restaurantPosts", restaurantRouter);
 app.use("/user", userRouter);
 app.use("/chat", chatroomRouter);
 
 mongoose
   .connect(process.env.MongoConnect)
-  .then(() => console.log("connected to MongoDB"))
-  .then(() => {
-    app.listen(process.env.Port, () => {
-      console.log(`server is running on port ${process.env.Port}`);
-    });
-  })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((error) => {
     console.error("Error connecting to MongoDB", error);
   });
 
 io.use(async (socket, next) => {
   try {
-    const token = socket.handshake.query.token;
-    const payload = await jwt.verify(token, process.env.SECRET);
+    const token = socket.handshake.auth.token;
+    const payload = jwt.verify(token, process.env.SECRET);
     socket.userId = payload.id;
     next();
-  } catch (err) {}
+  } catch (err) {
+    next(new Error("Authentication error"));
+  }
 });
 
-server.listen(process.env.CHAT_PORT, () => {
-  console.log(`Chat server is running on port ${process.env.CHAT_PORT}`);
-});
+io.on("connection", (socket) => {
+  console.log("Connected: " + socket.userId);
 
-io.on("connection", (Socket) => {
-  console.log("connected:" + Socket.userId);
-
-  Socket.on("disconnect", () => {
-    console.log("Disconnected: " + Socket.userId);
+  socket.on("disconnect", () => {
+    console.log("Disconnected: " + socket.userId);
   });
 });
+
+server.listen(process.env.Port, () => {
+  console.log(`Server is running on port ${process.env.Port}`);
+});
+
 app.get("/", (req, res) => {
   res.statusCode = 200;
   res.send("Connected from server side");
