8ab37511396d9558f8daca96c58944fe824b6dbb
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-07-13 00:07:20 +0100

    added message and user route and some sockets

--- Capstone-Project/Backend/index.js
+++ Capstone-Project/Backend/index.js
@@ -9,6 +9,10 @@ import { Server } from "socket.io";
 import { router as restaurantRouter } from "./Routes/router.js";
 import { router as userRouter } from "./Routes/user.js";
 import { router as chatroomRouter } from "./Routes/chatroom.js";
+import { router as messageRouter } from "./Routes/Messages.js";
+
+import { Messages } from "./Model/Message.js";
+import { User } from "./Model/User.js";
 
 dotenv.config();
 
@@ -33,7 +37,8 @@ app.use(express.json());
 
 app.use("/restaurantPosts", restaurantRouter);
 app.use("/user", userRouter);
-app.use("/chat", chatroomRouter);
+app.use("/groupchats", chatroomRouter);
+app.use("/groupchats/:chatId", messageRouter);
 
 mongoose
   .connect(process.env.MongoConnect)
@@ -41,25 +46,6 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to MongoDB", error);
   });
-
-io.use(async (socket, next) => {
-  try {
-    const token = socket.handshake.auth.token;
-    const payload = jwt.verify(token, process.env.SECRET);
-    socket.userId = payload.id;
-    next();
-  } catch (err) {
-    next(new Error("Authentication error"));
-  }
-});
-
-io.on("connection", (socket) => {
-  console.log("Connected: " + socket.userId);
-
-  socket.on("disconnect", () => {
-    console.log("Disconnected: " + socket.userId);
-  });
-});
 
 server.listen(process.env.Port, () => {
   console.log(`Server is running on port ${process.env.Port}`);
@@ -73,4 +59,56 @@ app.get("/", (req, res) => {
 app.all("*", (req, res) => {
   res.statusCode = 400;
   res.send("Path not found");
+});
+
+io.use(async (socket, next) => {
+  try {
+    const token = socket.handshake.auth.token;
+    console.log("token:" + token);
+    if (!token) {
+      throw new Error("no token provided");
+    }
+    const payload = await jwt.verify(token, process.env.SECRET);
+    console.log("payload: " + payload);
+    socket.userId = payload.id;
+    next();
+  } catch (err) {
+    next(new Error("Authentication error:" + err.message));
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected: " + socket.userId);
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected: " + socket.userId);
+  });
+
+  socket.on("joinRoom", ({ chatId }) => {
+    socket.join(chatId);
+    console.log("A user has joined the chat: " + chatId);
+  });
+
+  socket.on("leaveRoom", ({ chatId }) => {
+    socket.leave(chatId);
+    console.log("A user has left the chat: " + chatId);
+  });
+
+  socket.on("chatroomMessage", async ({ chatId, message }) => {
+    if (message.trim().length > 0) {
+      const user = await User.findOne({ _id: socket.userId });
+      const newMessage = new Messages({
+        chatroom: chatId,
+        user: socket.userId,
+        message,
+      });
+      io.to(chatId).emit("newMessage", {
+        message,
+        name: user.name,
+        userId: socket.userId,
+      });
+
+      await newMessage.save();
+    }
+  });
 });
