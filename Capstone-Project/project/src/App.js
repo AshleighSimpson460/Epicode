cccc7fe2b4a9804da2cff5c811dcc163f9454ec2
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-08-01 10:34:18 +0100

    added a fetch to identify user when onclicking nme

--- Capstone-Project/project/src/App.js
+++ Capstone-Project/project/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io, { Socket } from "socket.io-client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 import LoginPage from "./Components/UserPage/LoginPage.tsx";
 import RegisterPage from "./Components/UserPage/RegisterPage.tsx";
@@ -8,11 +9,37 @@ import NavBar from "./Components/NavBar.tsx";
 import ChatroomPage from "./Components/Chatroom/ChatroomPage.tsx";
 import IndexPage from "./Components/UserPage/indexPage.tsx";
 import Homepage from "./Components/Homepage.tsx";
-import GroupMessage from "./Components/Chatroom/GroupMessage.js";
-import { showToast } from "./Components/Toaster.js";
+import GroupMessage from "./Components/Chatroom/GroupMessage.tsx";
+import { showError, showToast } from "./Components/Toaster.js";
+import PrivateMessages from "./Components/PrivateMessage/PrivateMessage.tsx";
 
 function App() {
   const [socket, setSocket] = useState(null);
+  const [currentUser, setCurrentUser] = useState({
+    id: "",
+    name: "",
+  });
+
+  const fetchCurrentUser = async () => {
+    try {
+      const fetchUser = await fetch("http://localhost:3002/user");
+      const currentUserData = await fetchUser.json();
+
+      const id = currentUserData._id;
+      const name = currentUserData.name || "Unknown";
+
+      setCurrentUser({
+        id: id,
+        name: name,
+      });
+    } catch (error) {
+      console.error("Error fetching current user:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
 
   const setupSocket = () => {
     const token = localStorage.getItem("C_Token");
@@ -22,13 +49,20 @@ function App() {
         auth: {
           token: token,
         },
+        reconnection: false,
       });
+
+      const decodedToken = jwt_decode(token, { ignoreExpiration: true });
+      const user = { id: decodedToken.id, name: decodedToken.name };
+      console.log("Decoded User:", user);
+
+      setCurrentUser(user);
 
       console.log(newSocket);
       newSocket.on("disconnect", () => {
         setSocket(null);
         setTimeout(setupSocket, 6000);
-        showToast("success", "You have been disconnected");
+        showError("error", "It looks like you have been disconnected");
       });
 
       newSocket.on("connect", () => {
@@ -43,10 +77,6 @@ function App() {
     setupSocket();
     //eslint-disable-next-line
   }, []);
-
-  if (!socket) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div>
@@ -70,6 +100,16 @@ function App() {
             path="/groupchats/:chatId"
             element={
               <GroupMessage socket={socket} setupSocket={setupSocket} exact />
+            }
+          />
+          <Route
+            path="/inbox"
+            element={
+              <PrivateMessages
+                socket={socket}
+                setupSocket={setupSocket}
+                currentUser={currentUser}
+              />
             }
           />
         </Routes>
