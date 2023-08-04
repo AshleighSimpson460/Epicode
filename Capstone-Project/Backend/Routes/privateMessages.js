754218a5e5f723b9ff736bca76aaad78c4974fbd
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-08-04 14:31:50 +0100

    added new code for the routes to allow private mesaging

--- Capstone-Project/Backend/Routes/privateMessages.js
+++ Capstone-Project/Backend/Routes/privateMessages.js
@@ -1,46 +1,73 @@
 import express from "express";
 import { PrivateMessage } from "../Model/PrivateMessages.js";
-import { Messages } from "../Model/Message.js";
-
+import setCurrentUser from "../MiddleWare/setCurrentUser.js";
 import Auth from "../MiddleWare/Auth.js";
+import { generatePrivateId } from "../utils/generatePrivateId.js"; // Import the generatePrivateId function
+import mongoose from "mongoose";
+import { PrivateChatMessage } from "../Model/PrivateChatMessage.js";
 
 export const router = express.Router();
 
-router.get("/personal", Auth, async (req, res) => {
-  const { chatId } = req.params;
+router.use(setCurrentUser);
+
+router.get("/", Auth, async (req, res) => {
+  const { chatId } = req.query;
 
   try {
-    const privateMessage = await PrivateMessage.findOne({ chatId }).populate(
-      "participants",
-      "user"
-    );
+    const privateMessage = await PrivateMessage.findOne({ chatId }).populate({
+      path: "messages",
+      populate: {
+        path: "userId",
+        model: "User",
+        select: "name", // Select the name field of the user only
+      },
+      options: { sort: { timestamp: 1 } }, // Sort messages in ascending order based on timestamp
+    });
+
     if (!privateMessage) {
       return res.status(404).json({ error: "Private chat not found" });
     }
+
+    res.json(privateMessage);
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Server error" });
   }
 });
 
-router.post("/:chatId", Auth, async (req, res) => {
-  const { chatId, message, userId } = req.body;
+router.post("/:userId", Auth, async (req, res) => {
+  const { participants, message } = req.body; // Include "message" in the request body
 
-  const newMessage = new Messages({
-    chatroom: chatId,
-    user: userId,
-    message,
-  });
-  await newMessage.save();
+  console.log("currentuser:", req.currentUser.id);
 
-  io.to(chatId).emit("newMessage", {
-    message,
-    name: userId,
-    timestamp: new Date().toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    }),
-  });
+  try {
+    const participantIds = participants.map(
+      (participant) => new mongoose.Types.ObjectId(participant)
+    );
 
-  res.json({ message: "Private message sent successfully" });
+    participantIds.sort();
+
+    const chatId = generatePrivateId(participants[0], participants[1]);
+
+    // Check if the private chat already exists with these participants
+    const existingPrivateMessage = await PrivateMessage.findOne({
+      participants: { $all: participantIds },
+    });
+
+    if (!existingPrivateMessage) {
+      // Create a new private message only if it doesn't exist
+      const newPrivateChatMessage = new PrivateChatMessage({
+        chatId,
+        userId: participantIds,
+        message,
+      });
+
+      await newPrivateChatMessage.save();
+    }
+
+    res.json({ chatId });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
 });
