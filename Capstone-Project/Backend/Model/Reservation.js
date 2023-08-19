6b0bab17dce3c78241b9c3d2e26c366637ea63a5
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-08-19 12:24:39 +0100

    reservation model created

---
+++ Capstone-Project/Backend/Model/Reservation.js
@@ -0,0 +1,19 @@
+import { Timestamp } from "mongodb";
+import mongoose from "mongoose";
+
+const reservationSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+  guests: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const Reservation = mongoose.model("Reservation", reservationSchema);
