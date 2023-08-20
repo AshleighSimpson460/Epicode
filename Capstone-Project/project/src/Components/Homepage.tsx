20b57d088fa0c7777585ae3b6a647b9c2137e41a
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2023-08-20 21:01:00 +0100

    removed unused line

--- Capstone-Project/project/src/Components/Homepage.tsx
+++ Capstone-Project/project/src/Components/Homepage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Center, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Homepage = () => {
   return (
